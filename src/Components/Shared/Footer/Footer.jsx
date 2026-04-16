208cf8aa7ec271401a93052ee80f52803b528e92
Nishat Yeasmin <nishatyasminnisha@gmail.com>
2026-04-16 11:09:09 +0600

    fotter section added

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
-
+import footer from '../../../../public/image/logo-xl.png'
+import insta from '../../../../public/image/instagram.png'
+import facebook from '../../../../public/image/facebook.png'
+import twitter from '../../../../public/image/twitter.png'
 const Footer = () => {
     return (
-        <div>
-            
+        <div className='bg-green-950 '>
+            <div className='w-11/12 mx-auto py-10 '>
+                <div className='text-center flex flex-col items-center'>
+                <img src={footer} alt="Footer Logo" />
+
+                <p className='text-gray-400'>Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.</p>
+            </div>
+
+             <div className='text-center flex flex-col items-center mt-7'>
+                <h2 className=' text-white font-bold text-2xl mb-4'>Social Links</h2>
+
+                <div className='flex gap-3'>
+                        <img src={insta} alt="Instagram" />
+
+                        <img src={facebook} alt="Facebook" />
+
+                        <img src={twitter} alt="Twitter" />
+
+                </div>
+             </div>
+
+            <div className='text-gray-600 mt-5'>
+                <hr />
+            </div>
+
+            <div className='text-gray-500 flex justify-between mt-3.5 '>
+                <p>© 2026 KeenKeeper. All rights reserved.</p>
+
+                <div className='flex justify-between gap-4'>
+                    <p>Privacy Policy</p>
+                    <p>Terms of Service</p>
+                    <p>Cookies</p>
+                </div>
+            </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -8,10 +8,30 @@ const Home = () => {
               <h1 className='font-bold text-[#1F2937] text-4xl mb-3'>Friends to keep close in your life</h1>
             <p className='text-[#64748B] mb-3'>Your personal shelf of meaningful connections. Browse, tend, and nurture the <br/>
 relationships that matter most.</p>
+<button className='text-white bg-green-950 p-2 font-semibold'>+ Add a Friend</button>
           </div>
 
-<button className='text-white bg-green-950 p-2 font-semibold'>+ Add a Friend</button>
+           <div className='grid grid-cols-4 gap-6 w-11/15 mx-auto mt-7 '>
+            <div className='text-center bg-white border border-gray-100 rounded-2xl shadow-sm p-5 mt-7'>
+                <h1 className='font-bold text-2xl text-green-900'>10</h1>
+                <p>Total Friends</p>
+            </div>
+            <div className='text-center bg-white border border-gray-100 rounded-2xl shadow-sm p-5 mt-7'>
+                <h1 className='font-bold text-2xl text-green-900'>3</h1>
+                <p>On Track</p>
+            </div>
+            <div className='text-center bg-white border border-gray-100 rounded-2xl shadow-sm p-5  mt-7'>
+                <h1 className='font-bold text-2xl text-green-900'>6</h1>
+                <p>Need Attention</p>
+            </div>
+            <div className='w-full text-center bg-white border border-gray-100 shadow-sm rounded-2xl p-5  mt-7'>
+                <h1 className='font-bold text-2xl text-green-900'>12</h1>
+                <p>Interaction This Month</p>
+            </div>
+           </div>
         </div>
+
+       
     );
 };
 
